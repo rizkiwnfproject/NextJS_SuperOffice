327f43824eb1fec53d82dc3da73ccc88f5f40bb0
rizkiwnfproject <rizkiwahyuproject@gmail.com>
2025-10-03 10:43:33 +0700

    first commit

---
+++ src/app/(front)/testimonials/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const TestimonialPage = () => {
+  return (
+    <>
+      <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+        Testimonial
+      </div>
+    </>
+  );
+};
+
+export default TestimonialPage;
